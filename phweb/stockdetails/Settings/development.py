a63c7845ecaf5fee09ef6e24d8450cc78445a7af
Pradip <pradip.humane@covalensedigital.net>
2024-05-04 17:41:12 +0530

    added new changes

---
+++ phweb/stockdetails/Settings/development.py
@@ -0,0 +1,2 @@
+DEBUG = True
+APIURL = 'http://127.0.0.1:8001'
--- phweb/stockdetails/views.py
+++ phweb/stockdetails/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
-
 from django.template import loader
 from django.http import HttpResponse
+import requests
+
+from .Settings.development import APIURL
 
 
-def index(request):
+def index(request,symbol):
+    headers = {"Content-Type": "application/json"}
+    postData = { "stockdetails": { "symbol": symbol.upper()}}
+    stockDetailsapi = APIURL+'/stocksdetails/'
+    response = requests.post(stockDetailsapi, headers=headers, json=postData)
+    stockData = {}
+    for res in response.json():
+        stockData = res
+
     template = loader.get_template("stockdetails/stockinfo.html")
-    context = {}
-    return HttpResponse(template.render(context,request))
-    #return render(request, "stockdetails/stockinfo.html", context)
+    #print(response.json())
+    context = {"stockinfo":stockData}
+    #return HttpResponse(template.render(context,request))
+    return render(request, "stockdetails/stockinfo.html", context)
 
 # Create your views here.
